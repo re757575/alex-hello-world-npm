31f838af7eb8c019cbd28c39ff32c5665040386d
Alex.Dai <alex.dai@fortune-inc.com>
2020-08-20 14:38:20 +0800

    fix

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-import { VERSION  } from "lodash/_version";
+import  { defaults }  from 'lodash'
 
 function helloWorld() {
   console.log('Hello World!');
-  console.log(VERSION);
-  // var r = defaults({ 'a': 1 }, { 'a': 3, 'b': 2 });
-  // console.log(r);
-  return VERSION;
+
+  var r = defaults({ 'a': 1 }, { 'a': 3, 'b': 2 });
+  console.log(r);
 }
 
 export {
